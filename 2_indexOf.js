a1405d0e611fd906b6c43970f97f4cfecf8cb648
Kim Örnberg <kim.ornberg@skola.botkyrka.se>
2020-12-07 15:56:18 +0100

    includes & push_pop

--- 2_indexOf.js
+++ 2_indexOf.js
@@ -4,9 +4,29 @@
 // och returnerar indexet hos det elementet som matchar.
 // indexOf returnerar -1 om inget matchande element hittas i arrayen
 
+
+//IndexOF
 const things = ['car', 'house', 'castle', 'bridge', 'tree']
 
 console.log(things.indexOf('tree')) // 4
 console.log(things.indexOf('car')) // 0
 
 console.log(things.indexOf('spaceship')) // -1
+
+
+//includes
+const jdm = ["Rx7", "R34", "Supra", "NSX"]
+
+console.log(jdm.includes("Rx7"))
+
+console.log(jdm.includes("C63"))
+
+
+//push_pop
+
+const countries = ["Sweden", "Ecuador", "Russia", "Kazakstan"]
+
+let last = countries.pop()
+let first = countries.push("Indonesia")
+
+console.log(countries)
